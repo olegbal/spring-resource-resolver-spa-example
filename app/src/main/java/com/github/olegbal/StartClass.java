935c7e4259c0b7b86eff643d24fe86ba648e0f8a
olegbal <olegbalah@gmail.com>
2017-05-17 00:41:24 +0200

    clear startclass

--- app/src/main/java/com/github/olegbal/StartClass.java
+++ app/src/main/java/com/github/olegbal/StartClass.java
@@ -4,25 +4,11 @@ package com.github.olegbal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.File;
-
 @SpringBootApplication
 public class StartClass {
 
-    static private final String jarName = "static-data.jar";
 
     public static void main(String[] args) throws Exception {
-
-        PathOperator.getJarContainingFolder(StartClass.class);
-        if (System.getProperty("os.name").contains("Windows")) {
-            ProcessBuilder pb = new ProcessBuilder(System.getenv("JAVA_HOME") + "\\bin\\jar.exe", "-xf", Paths.executingPath + jarName, "resources/");
-            pb.directory(new File(Paths.executingPath));
-            pb.start();
-        } else {
-            ProcessBuilder pb = new ProcessBuilder("jar", "xf", Paths.executingPath + jarName, "resources/");
-            pb.directory(new File(Paths.executingPath));
-            pb.start();
-        }
         SpringApplication.run(StartClass.class, args);
     }
 }
