436e38b9d196efc85d0e9f916249ff84402e6eff
olegbal <olegbalah@gmail.com>
2017-05-17 00:42:03 +0200

    change Controller's logic

--- angular2-client/src/main/java/com/github/olegbal/angular/controllers/AngularClientController.java
+++ angular2-client/src/main/java/com/github/olegbal/angular/controllers/AngularClientController.java
@@ -1,19 +1,23 @@
 package com.github.olegbal.angular.controllers;
 
-import com.github.olegbal.Paths;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
-import java.io.*;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
 public class AngularClientController {
 
     @RequestMapping(path = "/changeclient/angular/", method = RequestMethod.GET)
-    public void changeOnAngularClient() throws IOException {
-        FileCopyUtils.copy(new File(Paths.executingPath + "/resources/angular/index.html"),
-                new File(Paths.executingPath + "/resources/index.html"));
+    public RedirectView changeOnAngularClient(HttpServletResponse response) throws IOException {
+
+        Cookie cookie = new Cookie("client", "angular");
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        return new RedirectView("/");
     }
 }
--- react-client/src/main/java/com/github/olegbal/react/controllers/ReactClientController.java
+++ react-client/src/main/java/com/github/olegbal/react/controllers/ReactClientController.java
@@ -1,20 +1,23 @@
 package com.github.olegbal.react.controllers;
 
-import com.github.olegbal.Paths;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
-import java.io.File;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RestController
 public class ReactClientController {
 
     @RequestMapping(path = "/changeclient/react/", method = RequestMethod.GET)
-    public void changeOnReactClient() throws IOException {
-        FileCopyUtils.copy(new File(Paths.executingPath + "/resources/react/index.html"),
-                new File(Paths.executingPath + "/resources/index.html"));
+    public RedirectView changeOnAngularClient(HttpServletResponse response) throws IOException {
+
+        Cookie cookie = new Cookie("client", "react");
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        return new RedirectView("/");
     }
 }
